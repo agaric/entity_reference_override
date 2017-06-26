9b2e6d1d95de5a0a3717a6552ee0f19f97b8143c
Benjamin Melançon <ben@agaric.com>
2017-06-26 12:14:09 -0400

    Add GitHub repositories

--- get_all_repositories.sh
+++ get_all_repositories.sh
@@ -5,5 +5,6 @@ do
   git clone git@gitlab.com:drutopia/$project.git
   cd $project
   git remote add drupal mlncn@git.drupal.org:project/$project.git
+  git remote add github git@github.com:drutopia/$project.git
   cd ..
 done < drutopia_projects.txt
--- update_drupal_org_from_gitlab.sh
+++ update_drupal_org_from_gitlab.sh
@@ -1,3 +1,5 @@
 for i in */.git; do ( echo $i; cd $i/..; git pull; ); done
 
 for i in */.git; do ( echo $i; cd $i/..; git push drupal 8.x-1.x; ); done
+
+for i in */.git; do ( echo $i; cd $i/..; git push github 8.x-1.x; ); done
