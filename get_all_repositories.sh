33ce4a1d987ec295e8371412d0cae43a932255a8
Benjamin Melançon <ben@agaric.com>
2017-07-03 11:23:10 -0400

    Add automation for fetching and pushing infrastructure projects (no 8.x branches)

--- get_all_repositories.sh
+++ get_all_repositories.sh
@@ -8,3 +8,13 @@ do
   git remote add github git@github.com:drutopia/$project.git
   cd ..
 done < drutopia_projects.txt
+
+while read project
+do
+  cd drutopia-infrastructure
+  git clone git@gitlab.com:drutopia/$project.git
+  cd $project
+  git remote add drupal mlncn@git.drupal.org:project/$project.git
+  git remote add github git@github.com:drutopia/$project.git
+  cd ../..
+done < drutopia_infrastructure_projects.txt
--- update_drupal_org_from_gitlab.sh
+++ update_drupal_org_from_gitlab.sh
@@ -3,3 +3,7 @@ for i in */.git; do ( echo $i; cd $i/..; git pull; ); done
 for i in */.git; do ( echo $i; cd $i/..; git push drupal 8.x-1.x; ); done
 
 for i in */.git; do ( echo $i; cd $i/..; git push github 8.x-1.x; ); done
+
+for i in drutopia-infrastructure/*/.git; do ( echo $i; cd $i/..; git push drupal master; ); done
+
+for i in drutopia-infrastructure/*/.git; do ( echo $i; cd $i/..; git push github master; ); done
