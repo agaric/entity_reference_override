3e5db542fa3378aec5a9fb11474394efffd17637
Benjamin Melançon <ben@agaric.com>
2017-10-24 10:33:57 -0400

    Push tags to drupal and github
    Not that this presumes you or someone has 'git push --tags' to get the tags
    on gitlab first.

--- update_drupal_org_github_from_gitlab.sh
+++ update_drupal_org_github_from_gitlab.sh
@@ -1,8 +1,8 @@
 for i in */.git; do ( echo $i; cd $i/..; git pull; ); done
 
-for i in */.git; do ( echo $i; cd $i/..; git push drupal 8.x-1.x; ); done
+for i in */.git; do ( echo $i; cd $i/..; git push drupal 8.x-1.x; git push drupal --tags; ); done
 
-for i in */.git; do ( echo $i; cd $i/..; git push github 8.x-1.x; ); done
+for i in */.git; do ( echo $i; cd $i/..; git push github 8.x-1.x; git push github --tags; ); done
 
 
 for i in drutopia-infrastructure/*/.git; do ( echo $i; cd $i/..; git pull; ); done
