1bd0cbabfbac2c65bfe8d7c186f13fa83a902254
Benjamin Melançon <ben@agaric.com>
2018-04-06 17:07:53 -0400

    Add option to tag for packagist to release tag script

--- tag_project_release.sh
+++ tag_project_release.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-if [ 2 -ne $# ]; then
+if [ 1 -ge $# ]; then
     echo "usage: $0 <project name> <release version>"
     echo "example: $0 drutopia_article 1.0-alpha1"
     exit 2
@@ -19,3 +19,16 @@ echo "Pushing tag to Github."
 git push github tag 8.x-$release;
 echo "Pushing tag to drupal.org."
 git push drupal tag 8.x-$release;
+
+
+if [ ! -z $3 ]
+then
+    echo "Tagging for packagist"
+    git tag $release;
+    echo "Pushing tag to GitLab."
+    git push origin tag $release;
+    echo "Pushing tag to Github."
+    git push github tag $release;
+    echo "Pushing tag to drupal.org."
+    git push github tag $release;
+fi
