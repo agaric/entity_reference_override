5de26a92f75f6581fdd8606befe0918a32a856bc
benjamin melançon <ben@agaric.com>
2018-07-31 14:43:56 -0400

    Add helper script to update all Drupal remotes
    per https://www.drupal.org/drupalorg/docs/user-accounts/git-authentication-for-drupalorg-projects

---
+++ update_drupal_org_remotes.sh
@@ -0,0 +1,6 @@
+# Temporary measure to update our local repos with Drupal.org's new way: https://www.drupal.org/gitauth 
+
+for i in */.git; do ( echo $i; cd $i/..; git remote set-url drupal git@git.drupal.org:project/$i.git; ); done
+
+for i in drutopia-infrastructure/*/.git; do ( echo $i; cd $i/..; git remote set-url drupal git@git.drupal.org:project/$i.git; ); done
+
