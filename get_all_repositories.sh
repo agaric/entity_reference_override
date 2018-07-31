20bdda88a6984e4274559d0902e4c31fdd2f25d2
benjamin melançon <ben@agaric.com>
2018-07-31 14:47:30 -0400

    Update remote URL used for Drupal
    Now that Drupal.org has joined the modern wold, this will be simpler for us.
    
    See  for updating your existing hackysync local

--- get_all_repositories.sh
+++ get_all_repositories.sh
@@ -1,16 +1,10 @@
 #! /bin/sh
 
-if [ 1 -ne $# ]; then
-    echo "usage: $0 <drupal.org user name>"
-    exit 2
-fi
-user=$1
-
 while read project
 do
   git clone git@gitlab.com:drutopia/$project.git
   cd $project
-  git remote add drupal $user@git.drupal.org:project/$project.git
+  git remote add drupal git@git.drupal.org:project/$project.git
   git remote add github git@github.com:drutopia/$project.git
   cd ..
 done < drutopia_projects.txt
@@ -22,7 +16,7 @@ do
   cd drutopia-infrastructure
   git clone git@gitlab.com:drutopia/$project.git
   cd $project
-  git remote add drupal $user@git.drupal.org:project/$project.git
+  git remote add drupal git@git.drupal.org:project/$project.git
   git remote add github git@github.com:drutopia/$project.git
   cd ../..
 done < drutopia_infrastructure_projects.txt
