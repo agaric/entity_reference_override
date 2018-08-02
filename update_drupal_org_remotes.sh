fa326bec60bc5aaef41a2ca35d0b2a76aedfc9a1
benjamin melançon <ben@agaric.com>
2018-08-02 07:51:27 -0400

    Fix d.o remotes for infrastructure projects too

--- update_drupal_org_remotes.sh
+++ update_drupal_org_remotes.sh
@@ -2,5 +2,5 @@
 
 for i in */.git; do ( echo $i; cd $i/..; git remote set-url drupal git@git.drupal.org:project/${i::-5}.git; ); done
 
-for i in drutopia-infrastructure/*/.git; do ( echo $i; cd $i/..; git remote set-url drupal git@git.drupal.org:project/$i.git; ); done
+for i in drutopia-infrastructure/*/.git; do ( echo $i; cd $i/..; git remote set-url drupal git@git.drupal.org:project/${i::-5}.git; ); done
 
