f1d6f95115e6d3dd92cb2887005069133a53cfc7
benjamin melançon <ben@agaric.com>
2019-11-14 14:33:43 -0500

    Remove no-longer-needed bridge script

--- update_drupal_org_remotes.sh
+++
@@ -1,6 +0,0 @@
-# Temporary measure to update our local repos with Drupal.org's new way: https://www.drupal.org/gitauth 
-
-for i in */.git; do ( echo $i; cd $i/..; git remote set-url drupal git@git.drupal.org:project/${i::-5}.git; ); done
-
-for i in drutopia-infrastructure/*/.git; do ( echo $i; cd $i/..; git remote set-url drupal git@git.drupal.org:project/${i::-5}.git; ); done
-
